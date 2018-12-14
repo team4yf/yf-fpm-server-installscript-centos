318bd6cd9332e1aca460ccc16c3f7d69802cc6e9
yfsoftcom <yfsoftcom@126.com>
2018-12-14 14:32:37 +0800

    add docker-compose to docker.script

--- yfci-docker.sh
+++ yfci-docker.sh
@@ -12,10 +12,30 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 
 yum list docker-ce --showduplicates | sort -r
 
-sudo yum install -y docker-ce
+yum install -y docker-ce
 
 systemctl start docker
 
 systemctl enable docker
 
 docker version
+
+# install docker-compose
+
+# 1.安装python-pip
+yum install python-pip
+# 2.无安装包
+yum -y install epel-release
+# 3.升级python-pip
+pip install --upgrade pip
+# 4.安装docker-compse
+pip install docker-compose
+# 5.查看版本号
+docker-compose -version
+# 6.卸载
+# pip uninstall docker-compose
+# 7.安装指定版本
+# pip install docker-compose=1.23.1
+
+
+
